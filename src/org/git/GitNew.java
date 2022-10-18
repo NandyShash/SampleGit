fc8c8b59612df61fa3733dadeeda96f58c3873ff
NandyShash <nanthuraj19@gmail.com>
2022-10-19 00:21:18 +0530

    Success

--- src/org/git/GitNew.java
+++ src/org/git/GitNew.java
@@ -1,5 +1,14 @@
 package org.git;
 
 public class GitNew {
+	
+	public static void main(String[] args) {
+		
+		String word="Hello";
+		
+		System.out.println(word);
+		
+		System.out.println("Welcome to git hub");
+	}
 
 }
